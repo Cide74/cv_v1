10a69ee5b8e10025e9d4b6ac3f4d8e2d8e52ccea
Régis BRUNET BLEC <regis.blec@gmail.com>
2022-03-22 11:24:20 +0100

    mise en place de la structure de l'application

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Voici mon CV en ligne en React. Cela me permet de mettre en pratique mes connaissances en React.
 
+react
+material ui
+react_html2pdf
+scss
--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ function App() {
   return (
     <div className="App">
       test
+      {/** react, material ui, react-html2pdf */}
+      {/** variable css */}
+
+      {/** colonne de gauche */}
+        {/** détails personnels - user */}
+        {/** compétences - skills */}
+        {/** langues - skills */}
+        {/** centre d'intérêts - interests */}
+
+      {/** colonne de droite */}
+      {/** profils - profils */}
+      {/** formation */}
+      {/** exp - experience */}
+
+      {/** light dark mode */}
+      {/** génération pdf */}
     </div>
   );
 }
